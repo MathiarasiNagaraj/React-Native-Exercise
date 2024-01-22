c47cb73e02398a1a6b7bff99a6c9b9e1c7da0a18
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2024-01-22 15:05:29 +0530

    template created

---
+++ Exercise/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text, SafeAreaView} from 'react-native'
+const App = () => {
+  return (
+    <View>
+      <Text>hai</Text>
+      <SafeAreaView><Text>Welcome this is template</Text></SafeAreaView>
+</View>
+  )
+}
+
+export default App
