15ae7baa785f6c8c7e9bd68d6893014ec2626f1b
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2024-01-25 17:48:03 +0530

    Bottom Navigation issue fixed

--- Exercise/src/components/BottomNavigator/BottomNavigator.tsx
+++ Exercise/src/components/BottomNavigator/BottomNavigator.tsx
@@ -1,44 +1,125 @@
 import BottomSheet from '@gorhom/bottom-sheet';
 import React, { useCallback, useMemo, useRef } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { HOME } from '../../messages/CommonMessages';
+import { colors } from '../../styles/colors';
+import { globalStyles } from '../../styles/globalStyle';
+import StarSVG from '../../assets/images/icons/star.svg';
+import { Product } from '../../interface/Products';
 
-const BottomNavigator = () => {
-  const bottomSheetRef = useRef<BottomSheet>(null);
+interface BottomNavigatorProps{
+    detail:Product
+}
+const BottomNavigator :React.FC<BottomNavigatorProps>= ({detail}) => {
+ 
+  const snapPoints = useMemo(() => ['35%', '48%'], []);
+  const sizes = (
+    <FlatList
+      horizontal
+      data={detail.available_sizes}
+      renderItem={({item}) => <Text style={styles.itemWrapper}>{item}</Text>}
+      keyExtractor={item => item}
+      style={styles.wrapper}
+    />
+  );
 
-  // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
 
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}
-      >
-        <View style={styles.content}>
-          <Text>Awesome 🎉</Text>
-          {/* Your long content here */}
+    <BottomSheet snapPoints={snapPoints}>
+    <View style={styles.textContainer}>
+      <Text style={styles.title}>{detail.product_name}</Text>
+      <View style={styles.priceWrapper}>
+        <View>
+          <Text style={styles.orangeText}>
+            {detail.price_details.currency_code}
+            {detail.price_details.current_price}
+          </Text>
+          <Text>
+            <Text style={styles.strikeText}>
+              {HOME.POPULAR_PRODUCTS.RM}{' '}
+              {detail.price_details.actual_price}{' '}
+            </Text>
+            <Text style={globalStyles.text}>
+              {detail.price_details.discount} OFF
+            </Text>
+          </Text>
         </View>
-      </BottomSheet>
-    </GestureHandlerRootView>
+        <View>
+          <StarSVG style={styles.Rating} />
+          <Text style={styles.Rating}>
+            {detail.rating_details.rating}/{detail.rating_details.scale}
+          </Text>
+        </View>
+      </View>
+    </View>
+    <View style={styles.sizeWrapper}>
+      <Text style={styles.text}>
+        {HOME.POPULAR_PRODUCTS.availableSizes}
+      </Text>
+      {sizes}
+    </View>
+  </BottomSheet>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    padding: 16,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-  },
+    wrapper: {
+        display: 'flex',
+        columnGap: 20,
+        alignSelf: 'center',
+    
+        rowGap: 20,
+      },
+      title: {
+        fontSize: 20,
+        color: colors.black,
+        fontWeight: '500',
+        marginBottom: 10,
+      },
+      priceWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+      },
+    
+      text: {
+        color: colors.grey,
+        fontSize: 17,
+      },
+      textContainer: {
+        backgroundColor: colors.white,
+        padding: 22,
+      },
+    
+      orangeText: {
+        color: '#e2801c',
+        fontSize: 21,
+        fontWeight: '700',
+        marginVertical: 5,
+      },
+      Rating: {
+        textAlign: 'right',
+        marginTop: 10,
+      },
+    
+      strikeText: {
+        textDecorationLine: 'line-through',
+      },
+      itemWrapper: {
+        color: colors.black,
+        marginLeft: 25,
+        fontSize: 17,
+        fontWeight: '600',
+      },
+      sizeWrapper: {
+        backgroundColor: colors.lightestGrey,
+        height: 70,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 40,
+      },
 });
 
 export default BottomNavigator;
--- Exercise/src/screens/Product/Product.tsx
+++ Exercise/src/screens/Product/Product.tsx
@@ -14,40 +14,32 @@ import {useContext, useMemo} from 'react';
 import DressHangerSVG from '../../assets/images/icons/dressHanger.svg';
 import HeartSVG from '../../assets/images/icons/heart.svg';
 import LeftArrowSVG from '../../assets/images/icons/leftArrow.svg';
-import StarSVG from '../../assets/images/icons/star.svg';
+
 import Cart from '../../components/Cart/Cart';
 import {CartContext} from '../../context/CartContext';
 import {colors} from '../../styles/colors';
 import {globalStyles} from '../../styles/globalStyle';
 import {HOME} from '../../messages/CommonMessages';
 import { useNavigation } from '@react-navigation/native';
+import BottomNavigator from '../../components/BottomNavigator/BottomNavigator';
 
 export const Product = ({route}) => {
   const {detail} = route.params;
-  const snapPoints = useMemo(() => ['40%', '60%'], []);
+  const snapPoints = useMemo(() => ['20%', '50%'], []);
   const { addToCart } = useContext(CartContext);
   const navigation = useNavigation();
 
   const handleBackClick = () => {
     navigation.navigate('Home');
   };
-  const sizes = (
-    <FlatList
-      horizontal
-      data={detail.available_sizes}
-      renderItem={({item}) => <Text style={styles.itemWrapper}>{item}</Text>}
-      keyExtractor={item => item}
-      style={styles.wrapper}
-    />
-  );
 
   const onAddToCartHandler = () => {
     addToCart(detail.id);
   };
 
   return (
-    <SafeAreaView>
-      <ScrollView style={{position:'relative',backgroundColor:'pink'}}>
+
+      <ScrollView style={styles.container}>
         <View>
           <Image style={styles.img} source={{uri: detail.modelImg}} />
           <View style={styles.iconWrapper}>
@@ -61,40 +53,8 @@ export const Product = ({route}) => {
             </View>
           </View>
         </View>
-        {/* <BottomSheet style={{backgroundColor: 'red'}} snapPoints={snapPoints}> */}
-          <View style={styles.textContainer}>
-            <Text style={styles.title}>{detail.product_name}</Text>
-            <View style={styles.priceWrapper}>
-              <View>
-                <Text style={styles.orangeText}>
-                  {detail.price_details.currency_code}
-                  {detail.price_details.current_price}
-                </Text>
-                <Text>
-                  <Text style={styles.strikeText}>
-                    {HOME.POPULAR_PRODUCTS.RM}{' '}
-                    {detail.price_details.actual_price}{' '}
-                  </Text>
-                  <Text style={globalStyles.text}>
-                    {detail.price_details.discount} OFF
-                  </Text>
-                </Text>
-              </View>
-              <View>
-                <StarSVG style={styles.Rating} />
-                <Text style={styles.Rating}>
-                  {detail.rating_details.rating}/{detail.rating_details.scale}
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.sizeWrapper}>
-            <Text style={styles.text}>
-              {HOME.POPULAR_PRODUCTS.availableSizes}
-            </Text>
-            {sizes}
-          </View>
-        {/* </BottomSheet> */}
+        <BottomNavigator detail={detail}/>
+    
         <View style={styles.bottomWrapper}>
           <View style={styles.greyIcon}>
             <HeartSVG height={30} width={28} />
@@ -111,21 +71,18 @@ export const Product = ({route}) => {
           </TouchableOpacity>
         </View>
       </ScrollView>
-    </SafeAreaView>
+   
   );
 };
 const styles = StyleSheet.create({
+  container: {
+
+  },
   img: {
-    height: 600,
+    height: 635,
     width: '100%',
   },
-  wrapper: {
-    display: 'flex',
-    columnGap: 20,
-    alignSelf: 'center',
 
-    rowGap: 20,
-  },
   iconWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -133,14 +90,6 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 20,
     position: 'absolute',
-  },
-  icon: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 57,
-    width: 57,
-    borderRadius: 999,
-    backgroundColor: colors.white,
   },
   greyIcon: {
     justifyContent: 'center',
@@ -150,61 +99,17 @@ const styles = StyleSheet.create({
     borderRadius: 999,
     backgroundColor: colors.lightGrey,
   },
-  title: {
-    fontSize: 20,
-    color: colors.black,
-    fontWeight: '500',
-    marginBottom: 10,
-  },
-  priceWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-  },
-
-  text: {
-    color: colors.grey,
-    fontSize: 17,
-  },
-  textContainer: {
-    backgroundColor: colors.white,
-    padding: 22,
-  },
-
-  orangeText: {
-    color: '#e2801c',
-    fontSize: 21,
-    fontWeight: '700',
-    marginVertical: 5,
-  },
-  Rating: {
-    textAlign: 'right',
-    marginTop: 10,
-  },
-
-  strikeText: {
-    textDecorationLine: 'line-through',
-  },
-  itemWrapper: {
-    color: colors.black,
-    marginLeft: 25,
-    fontSize: 17,
-    fontWeight: '600',
-  },
-  sizeWrapper: {
-    backgroundColor: colors.lightestGrey,
-    height: 70,
-    display: 'flex',
-    flexDirection: 'row',
+  icon: {
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 40,
+    height: 57,
+    width: 57,
+    borderRadius: 999,
+    backgroundColor: colors.white,
   },
+
   bottomWrapper: {
-    // position: 'absolute',
-    // bottom: 0,
-    // right: 0,
-    // left:0,
+   
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems:'center',
